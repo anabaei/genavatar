16cff8651bd4a6777f3b9bde556a61092fa95584
anabaei <nabaei17@gmail.com>
2023-09-03 21:10:17 -0700

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='Header'>
+        
+      </div>
+      <div className='Main'>
+        <div className='side-bar'>
+          <Button variant="primary">Primary</Button>
+        </div>
+        <div className='feed'>
+          <Button variant="primary">Primary</Button>
+        </div>
+    
+      </div>
+    </div>
+  );
+}
+
+export default App;
